453e0a1e6863525b43b9ed891f754e20c0fa50eb
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-02 19:41:05 +0100

    add csrf

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -5,6 +5,8 @@
 
         <form class="col-6" action="{{ route('comics.store') }}" method="POST" enctype="multipart/form-data">
 
+            @csrf
+
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" name="title" id="title" class="form-control" placeholder="Insert New Title Here"
