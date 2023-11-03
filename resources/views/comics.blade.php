15f1df3c75e03d6a3be1f7c812d454375469af5f
MatteoNocera <matteo_nocera@tiscali.it>
2023-11-03 09:22:21 +0100

    add footer

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,12 +6,12 @@
 @section('content')
 
     <div class="container">
-        <h1>Comics Page</h1>
+        <h1 class="py-5 display-3 fw-bold">Comics Page</h1>
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-5">
             @foreach ($comics as $comic)
                 <div class="col">
-                    <div class="card h-100 text-center shadow border-0">
+                    <div class="card h-100 text-center shadow-lg border-0">
                         <div class="card-img-top">
                             @if (str_contains($comic->thumb, 'http'))
                                 <img class="my_card img-fluid" src="{{ $comic->thumb }}" alt="">
@@ -25,6 +25,9 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+        <div class="text-center">
+            <a href="{{ route('welcome') }}" class="btn btn-warning my-5 shadow">Go to Home</a>
         </div>
     </div>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,36 @@
-<footer>
-
+<footer class="bg-dark bg-gradient text-white py-5">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class="col">
+                <h3>
+                    Lorem.
+                    <ul class="list-unstyled">
+                        <li><a href="#">Lorem, ipsum.</a></li>
+                        <li><a href="#">Reprehenderit, accusantium?</a></li>
+                        <li><a href="#">Maiores, autem.</a></li>
+                    </ul>
+                </h3>
+            </div>
+            <div class="col text-center">
+                <h3>
+                    Necessitatibus!
+                    <ul class="list-unstyled">
+                        <li><a href="#">Provident, eligendi!</a></li>
+                        <li><a href="#">A, eum?</a></li>
+                        <li><a href="#">Provident, ullam!</a></li>
+                    </ul>
+                </h3>
+            </div>
+            <div class="col text-end">
+                <h3>
+                    Incidunt!
+                    <ul class="list-unstyled">
+                        <li><a href="#">Labore, amet.</a></li>
+                        <li><a href="#">A, voluptatem.</a></li>
+                        <li><a href="#">Totam, nihil?</a></li>
+                    </ul>
+                </h3>
+            </div>
+        </div>
+    </div>
 </footer>
